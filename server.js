98f7abf8bb914c6ec2109f14e7352493394977e3
Jeff Lam <jefflam1200@gmail.com>
2016-07-11 16:26:26 -0700

    Created a html page

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ var http = require('http');
 
 
 var app = express();
-
 port = process.env.PORT || 1337;
+//look for html, css, js, img files
+app.use(express.static(__dirname + "/client"));
 
+
+// app.get('/', function(req, res) {
+//   res.send("hello world from server.js")
+// });
 
 app.listen(port);
 console.log("server running on ", port )
-
-app.get('/', function(req, res) {
-  res.send("hello world from server.js")
-});
-
