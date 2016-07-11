3da57a9d38e295ce12c3a0cf036828c6fe530a15
Jeff Lam <jefflam1200@gmail.com>
2016-07-11 14:52:49 -0700

    Initial Commit

---
+++ Server/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var http = require('http');
+
+
+var app = express();
+
+port = process.env.PORT || 1337;
+
+var server = http.createServer(function(req,res){
+  res.writeHeader(200,{"Content-Type": "text/plain"});
+  res.write("Heyooo");
+  res.end();
+})
+server.listen(port);
+console.log("server running on ", port )
