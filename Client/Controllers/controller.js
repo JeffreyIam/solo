c45d36c80c213f1207bb44ffc39513fab2464f16
Jeff Lam <jefflam1200@gmail.com>
2016-07-11 19:43:47 -0700

    Successfully added get request to retrieve data from mongodb database

--- Client/Controllers/controller.js
+++ Client/Controllers/controller.js
@@ -2,4 +2,13 @@
 var myFood = angular.module('myFood',[]);
 myFood.controller('FoodCtrl',function($scope, $http) {
   console.log("Hey! It's me from controller!");
+
+  $http.get('/foodlist').success(function(res,req) {
+    console.log("got data req");
+    $scope.foodlist = res;
+  })
+
+
+
+
 });
--- server.js
+++ server.js
@@ -2,12 +2,23 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var http = require('http');
-
+var mongojs = require('mongojs');
+var db = mongojs('foodlist', ['foodlist']);
 
 var app = express();
 port = process.env.PORT || 1337;
 //look for html, css, js, img files
 app.use(express.static(__dirname + "/Client"));
+
+app.get('/foodlist', function(req, res) {
+  console.log("get req");
+
+  db.foodlist.find(function(err,data) {
+    console.log(data);
+    res.json(data);
+  });
+
+});
 
 
 app.listen(port);
