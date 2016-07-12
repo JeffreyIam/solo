84bade0f06df32bf97155e41dddd19d7f3ddf382
Jeff Lam <jefflam1200@gmail.com>
2016-07-11 20:01:28 -0700

    Fixing heroku problems

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ var http = require('http');
 var mongojs = require('mongojs');
 var db = mongojs('foodlist', ['foodlist']);
 
+
 var app = express();
-port = process.env.PORT || 1337;
+var port = process.env.MONGODB_URI || "mongodb://localhost/solodolo";
 //look for html, css, js, img files
 app.use(express.static(__dirname + "/Client"));
+mongoose.connect(port);
 
 app.get('/foodlist', function(req, res) {
   db.foodlist.find(function(err,data) {
@@ -17,5 +19,7 @@ app.get('/foodlist', function(req, res) {
 
 });
 
-app.listen(port);
-console.log("server running on ", port )
+var pert = process.env.PORT || 1337;
+app.listen(pert, function() {
+  console.log("server running on ", pert);
+});
